6325743c2649131a726c98233aa64644811a7117
Mateo Mcpherson <mateomcpherson@Mateos-MacBook-Pro.local>
2024-07-31 12:25:01 -0400

    Created Navbar

--- fitness-app/src/App.js
+++ fitness-app/src/App.js
@@ -5,19 +5,18 @@ import { Box } from "@mui/material";
 import ExerciseDetail from "./pages/ExerciseDetail";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fitness App </h1>
-      <Box width="400px">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/exercise/:id" element={<ExerciseDetail />} />
-        </Routes>
-      </Box>
-    </div>
+    <Box width="400px" sx={{ width: { x1: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
--- fitness-app/src/pages/ExerciseDetail.js
+++ fitness-app/src/pages/ExerciseDetail.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ExerciseDetails = () => {
-  return <div>ExerciseDetails</div>;
+  return (
+    <div>
+      <h1>ExerciseDetails</h1>
+    </div>
+  );
 };
 
 export default ExerciseDetails;
